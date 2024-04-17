f32042c167125d92e422d580712a7c8e20e43dee
David <schauppdavid@gmail.com>
2024-04-17 15:06:59 +0200

    stage_00_sft

---
+++ src/utils/trainer.py
@@ -0,0 +1,44 @@
+import os
+from transformers import TrainingArguments
+from trl import SFTTrainer
+import os
+import wandb
+from accelerate import Accelerator
+
+os.environ["WANDB_PROJECT"]="Self Rewarding Language Models"
+
+class Trainer:
+    def __init__(self):
+        self.output_dir = './results'
+
+    def train(self, model, tokenizer, lora_config, dataset, accelerator):
+        learning_rate = 2e-4
+        batch_size = 4
+        max_seq_length = 1024
+
+        training_args = TrainingArguments(
+            output_dir=self.output_dir,
+            per_device_train_batch_size=batch_size,
+            learning_rate=learning_rate,
+            gradient_accumulation_steps=4,
+            warmup_steps=30,
+            logging_steps=1,
+            num_train_epochs=1,
+            save_steps=50,
+            report_to="wandb",)
+
+        trainer = SFTTrainer(
+            model=model,
+            train_dataset=dataset,
+            peft_config=lora_config,
+            max_seq_length=max_seq_length,
+            tokenizer=tokenizer,
+            args=training_args,
+            dataset_text_field="text")
+
+        model, trainer = accelerator.prepare(model, trainer)
+
+        trainer.train()
+
+        output_dir = os.path.join(self.output_dir, "model")
+        trainer.model.save_pretrained(output_dir)
