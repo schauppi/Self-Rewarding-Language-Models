a815fcd938fd8ceffb806bb08bcd14dbbe6c2951
David <schauppdavid@gmail.com>
2024-05-13 08:27:09 +0200

    removed comments in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Create a Python 3.11 virtual environment
-#python3.11 -m venv self_rewarding_language_models
+python3.11 -m venv self_rewarding_language_models
 
 # Activate the virtual environment
-#source self_rewarding_language_models/bin/activate
+source self_rewarding_language_models/bin/activate
 
 # Install requirements from requirements.txt
-#pip install -r requirements.txt
+pip install -r requirements.txt
 
 # Download the dataset
 git clone https://huggingface.co/datasets/schauppi/srlm
